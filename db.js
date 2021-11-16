4c33d320504e577150a9f666ab8a9b64d0592539
jcharfauros <julia.charfauros@gmail.com>
2021-11-15 22:13:44 -0500

    marco fixed this

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
